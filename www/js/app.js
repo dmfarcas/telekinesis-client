0fff89648b7d74adbd01f8ebe509f0c8c9294117
dmfarcas <dan@farcas.me>
2016-01-20 00:39:10 +0200

    Added more mouse events.

--- www/js/app.js
+++ www/js/app.js
@@ -57,15 +57,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
   .state('app.touchpad', {
       url: '/touchpad',
       views: {
@@ -74,26 +65,9 @@ angular.module('starter', ['ionic', 'starter.controllers'])
           controller: 'TouchpadCtrl'
         }
       }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+    });
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/touchpad');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,52 +12,59 @@ angular.module('starter.controllers', [])
 
 })
 
-
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
 .controller('TouchpadCtrl', function($scope, $rootScope, $ionicGesture, socket) {
-  $scope.onGesture = function(gesture) {
-    $scope.gesture = gesture;
-    console.log(gesture);
-  };
+  var touchpad = angular.element(document.querySelector('#touchpad'));
 
-  var element = angular.element(document.querySelector('#content'));
   $ionicGesture.on('dragstart', function(e){
     $scope.$apply(function() {
       socket.emit('dragstart', {});
       console.log('Dragstart');
     });
 
-  }, element);
+  }, touchpad);
   $ionicGesture.on('dragend', function(e){
     $scope.$apply(function() {
       socket.emit('dragend', {});
       console.log('Dragend');
     });
+  }, touchpad);
 
-  }, element);
+  $ionicGesture.on('hold', function(e){
+    $scope.$apply(function() {
+      socket.emit('hold', {});
+      console.log('Hold.');
+    });
+  }, touchpad);
+
+  $ionicGesture.on('touch', function(e){
+    $scope.$apply(function() {
+      socket.emit('touch', {});
+      console.log('Touch.');
+    });
+  }, touchpad);
+
+  $ionicGesture.on('release', function(e){
+    $scope.$apply(function() {
+      socket.emit('release', {});
+      console.log('Release.');
+    });
+  }, touchpad);
+
+  $ionicGesture.on('tap', function(e){
+    $scope.$apply(function() {
+      socket.emit('tap', {});
+      console.log('Tapped.');
+    });
+  }, touchpad);
 
   $scope.data = {
      tapX : "",
      tapY : ""
    };
   $scope.dragEvent = function(event) {
-      $scope.data.tapX = event.gesture.touches[0].clientX;
-      $scope.data.tapY = event.gesture.touches[0].clientY;
+      $scope.data.tapX = Math.floor(event.gesture.touches[0].screenX);
+      $scope.data.tapY = Math.floor(event.gesture.touches[0].screenY);
       socket.emit('dragging', {x: $scope.data.tapX, y: $scope.data.tapY});
     };
 
-  })
-.controller('PlaylistCtrl', function($scope, $stateParams, $rootScope) {
-  $rootScope.toggledrag = true;
-
-});
+  });
